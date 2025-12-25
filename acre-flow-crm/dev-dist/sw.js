2ad1869b8aab7fd9c9e5b44c4e07dd8545ef6878
amantiwari2357 <amankumartiwari5255@gmail.com>
2025-12-25 12:57:01 +0530

    sx

--- acre-flow-crm/dev-dist/sw.js
+++ acre-flow-crm/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.ncejlb6o33"
+    "revision": "0.kujap3cfqm8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
