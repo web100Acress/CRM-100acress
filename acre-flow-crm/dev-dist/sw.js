15149e4bce2ea09db0cbf00091ea948f90ee388f
amantiwari2357 <amankumartiwari5255@gmail.com>
2026-01-02 16:26:11 +0530

    Quick check (fastest) Tum abhi ek test call karke mujhe paste karo:
    Browser console ka line: Response status: ...
    Status: Call testing steps ready; message testing ke liye provider/type confirm chahiye.

--- acre-flow-crm/dev-dist/sw.js
+++ acre-flow-crm/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.1vib3rce7q"
+    "revision": "0.q3m8cbjruh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
