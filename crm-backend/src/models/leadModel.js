d785d2a9c65e2d95a9182d4a64f57f09d3ab69c0
digiindia2025 <amantiwariat2005@gmail.com>
2026-01-23 00:17:23 +0530

    xa

--- crm-backend/src/models/leadModel.js
+++ crm-backend/src/models/leadModel.js
@@ -55,7 +55,7 @@ const leadSchema = new mongoose.Schema({
   phone: { type: String },
   status: {
     type: String,
-    enum: ['Cold', 'Warm', 'Hot', 'Not Interested', 'Closed'], // <-- match your frontend exactly
+    enum: ['Cold', 'Warm', 'Hot'], // <-- match your frontend exactly
     default: 'Cold'
   },
   location: String,
